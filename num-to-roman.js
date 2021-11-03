2f4fa6c77c8dc45811828719f26c9778166336c7
Anggaysm <prasetiaangga28@gmail.com>
2021-11-03 22:09:24 +0700

    add code

--- num-to-roman.js
+++ num-to-roman.js
@@ -1,26 +1,27 @@
 function convertToRoman(num) {
     const roman = {
-        M: 1000,
-        CM: 900,
-        D: 500,
-        CD: 400,
-        C: 100,
-        XC: 90,
-        L: 50,
-        XL: 40,
-        X: 10,
-        IX: 9,
-        V: 5,
-        IV: 4,
-        I: 1
+      1: "I",
+      4: "IV",
+      5 : "V",
+      9 : "IX",
+      10 : "X",
+      40 : "XL",
+      50 : "L",
+      90 : "XC",
+      100 : "C",
+      400 : "CD",
+      500 : "D",
+      900 : "CM",
+      1000: "M",
       };
       let str = '';
-    
-      for (let i of Object.keys(roman)) {
-        let q = Math.floor(num / roman[i]);
-        num -= q * roman[i];
-        str += i.repeat(q);
-      }
+      const rumus = Object.keys(roman).reverse()
+      rumus.forEach((key) =>{
+        while (key <= num){
+          str += roman[key];
+          num -= key;
+        }
+      })
     
       return str;
    }
